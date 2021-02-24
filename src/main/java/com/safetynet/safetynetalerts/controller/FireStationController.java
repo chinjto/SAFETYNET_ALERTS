e39057e40c6a7fdf36479c8e94192d1af6e2d73f
s6raphin <s6raphin@laposte.net>
2021-02-24 14:51:47 +0100

    modifications controller

--- src/main/java/com/safetynet/safetynetalerts/controller/FireStationController.java
+++ src/main/java/com/safetynet/safetynetalerts/controller/FireStationController.java
@@ -1,18 +1,19 @@
 package com.safetynet.safetynetalerts.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FireStationController {
 
     @GetMapping("/firestations/adress")
-    public String adress(){
+    public String adress(@RequestParam String adress){
         return adress();
     }
 
     @GetMapping("/firestations/station_number")
-    public int stationNumber(){
+    public int stationNumber(@RequestParam String stationNumber){
         return stationNumber();
     }
 }
