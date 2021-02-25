26dce33b439dcdc7b41a34e2802a5570ef34aa30
s6raphin <s6raphin@laposte.net>
2021-02-25 16:48:15 +0100

    Création des controller

--- src/main/java/com/safetynet/safetynetalerts/controller/FireStationController.java
+++ src/main/java/com/safetynet/safetynetalerts/controller/FireStationController.java
@@ -1,19 +1,10 @@
 package com.safetynet.safetynetalerts.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FireStationController {
 
-    @GetMapping("/firestations/adress")
-    public String adress(@RequestParam String adress){
-        return adress();
-    }
-
-    @GetMapping("/firestations/station_number")
-    public int stationNumber(@RequestParam String stationNumber){
-        return stationNumber();
-    }
 }
