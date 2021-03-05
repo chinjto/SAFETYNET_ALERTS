9d675cdc36d366f78e5e21ac4f1888ffca4af069
s6raphin <s6raphin@laposte.net>
2021-03-05 16:42:42 +0100

    lecture fichier Json

--- src/main/java/com/safetynet/safetynetalerts/configuration/DataConfiguration.java
+++ src/main/java/com/safetynet/safetynetalerts/configuration/DataConfiguration.java
@@ -1,6 +1,5 @@
 package com.safetynet.safetynetalerts.configuration;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -8,7 +7,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
 
 @Configuration
-@JsonAutoDetect
 public class DataConfiguration {
 
     @Bean
--- src/main/java/com/safetynet/safetynetalerts/repository/PersonRepository.java
+++ src/main/java/com/safetynet/safetynetalerts/repository/PersonRepository.java
@@ -2,11 +2,12 @@ package com.safetynet.safetynetalerts.repository;
 
 import com.safetynet.safetynetalerts.entity.Person;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
