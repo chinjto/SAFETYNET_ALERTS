49b1f891ab627e26ad8693de052fbd97af46acb8
s6raphin <s6raphin@laposte.net>
2021-03-23 19:06:41 +0100

    #19 Nettoyage de la branche develop
    Création des Id et des jointures pour les entity (plantage au run sans ces points)

--- src/main/java/com/safetynet/safetynetalerts/entity/FireStationEntity.java
+++ src/main/java/com/safetynet/safetynetalerts/entity/FireStationEntity.java
@@ -4,17 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class FireStationEntity {
 
+    @Id
     private String address;
 
     private String station;
 
+    @OneToMany
     private List<PersonEntity> persons;
 
 }
--- src/main/java/com/safetynet/safetynetalerts/entity/PersonEntity.java
+++ src/main/java/com/safetynet/safetynetalerts/entity/PersonEntity.java
@@ -4,12 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.io.Serializable;
 import java.util.List;
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class PersonEntity {
+public class PersonEntity implements Serializable {
+
+    @Id
+    private Long id;
 
     private String firstName;
 
@@ -25,8 +34,10 @@ public class PersonEntity {
 
     private String email;
 
+    @OneToMany
     private List<MedicalRecordEntity> medicalRecords;
 
+    @ManyToOne
     private FireStationEntity fireStation;
 
 }
