26ab65844b2e669368822d7056ce9aa3e11aaf67
s6raphin <s6raphin@laposte.net>
2021-03-23 18:15:53 +0100

    #19 Netoyage de la branche develop
    Suppression des tests vides (à recréer si besoin de faire de vrais tests)

--- src/test/java/com/safetynet/safetynetalerts/controller/ChildAlertControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.safetynet.safetynetalerts.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ChildAlertControllerTest {
-
-}
--- src/test/java/com/safetynet/safetynetalerts/mapper/CommunityEmailToPersonMapperTest.java
+++
@@ -1,7 +0,0 @@
-package com.safetynet.safetynetalerts.mapper;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CommunityEmailToPersonMapperTest {
-
-}
