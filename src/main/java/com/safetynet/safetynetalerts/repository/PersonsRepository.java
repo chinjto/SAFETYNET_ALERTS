09a430182dee57a85d21a82a71f60b11f3b788f5
s6raphin <s6raphin@laposte.net>
2021-03-24 12:14:01 +0100

    #20 Création repository "Persons"
    Modofication PersonsRepository de JPA en CRUD repository.

--- src/main/java/com/safetynet/safetynetalerts/repository/PersonsRepository.java
+++ src/main/java/com/safetynet/safetynetalerts/repository/PersonsRepository.java
@@ -1,11 +1,13 @@
 package com.safetynet.safetynetalerts.repository;
 
 import com.safetynet.safetynetalerts.entity.PersonEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Id;
+
 @Repository
-public interface PersonsRepository extends JpaRepository<PersonEntity,String> {
+public interface PersonsRepository extends CrudRepository<PersonEntity, Id> {
 
 
     
