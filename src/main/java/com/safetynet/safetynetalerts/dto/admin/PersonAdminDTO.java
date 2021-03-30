391262a513deabafb26881c4b52aa1b407cbb48b
s6raphin <s6raphin@laposte.net>
2021-03-30 14:04:32 +0200

    #20 Création repository "Persons"
    Création DTO

--- src/main/java/com/safetynet/safetynetalerts/dto/admin/PersonAdminDTO.java
+++ src/main/java/com/safetynet/safetynetalerts/dto/admin/PersonAdminDTO.java
@@ -3,13 +3,18 @@ package com.safetynet.safetynetalerts.dto.admin;
 import com.safetynet.safetynetalerts.entity.PersonEntity;
 import com.safetynet.safetynetalerts.service.admin.PersonServiceAdmin;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 public class PersonAdminDTO {
+
+    @Id
+    private Long id;
 
     private String firstName;
 
@@ -24,13 +29,5 @@ public class PersonAdminDTO {
     private String phone;
 
     private String email;
-
-    public static PersonServiceAdmin create(final PersonEntity item) {
-        return null;
-    }
-
-    public static PersonServiceAdmin update() {
-        return null;
-    }
 
 }
