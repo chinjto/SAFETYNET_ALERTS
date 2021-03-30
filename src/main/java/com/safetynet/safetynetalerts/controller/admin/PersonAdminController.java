4b03bc082cc318b82189da8032a1cb3caf705c1d
s6raphin <s6raphin@laposte.net>
2021-03-30 14:04:03 +0200

    #20 Création repository "Persons"
    Création controller

--- src/main/java/com/safetynet/safetynetalerts/controller/admin/PersonAdminController.java
+++ src/main/java/com/safetynet/safetynetalerts/controller/admin/PersonAdminController.java
@@ -1,37 +1,71 @@
 package com.safetynet.safetynetalerts.controller.admin;
 
 import com.safetynet.safetynetalerts.dto.admin.PersonAdminDTO;
-import com.safetynet.safetynetalerts.service.admin.PersonServiceAdmin;
-import lombok.NonNull;
+import com.safetynet.safetynetalerts.entity.PersonEntity;
+import com.safetynet.safetynetalerts.mapper.PersonMapper;
+import com.safetynet.safetynetalerts.repository.PersonsRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/persons")
 public class PersonAdminController {
 
-    @NonNull
-    private PersonServiceAdmin service;
+//    @NonNull
+//    private PersonServiceAdmin service;
+//
+//    @GetMapping()
+//    public Iterable<PersonAdminDTO> readAllPerson() {
+//        return service.readAll();
+//    }
+//
+//    @PostMapping()
+//    public PersonAdminDTO createPerson(@RequestBody PersonAdminDTO item) {
+//        return service.create(item);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public PersonAdminDTO updatePerson(@PathVariable Long id, @RequestBody PersonAdminDTO item) {
+//        return service.update(id, item);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public Boolean deletePerson(@PathVariable Long id) {
+//        return service.delete(id);
+//    }
 
-    @GetMapping()
-    public Iterable<PersonAdminDTO> readAllPerson() {
-        return service.readAll();
+    @Autowired
+    PersonsRepository personsRepository;
+
+    @Autowired
+    PersonMapper personMapper;
+
+    @GetMapping("/findAll")
+    public List<PersonAdminDTO> findAll() {
+
+        List<PersonEntity> findAll = personsRepository.findAll();
+        return personMapper.entityToDto(findAll);
+
     }
+    @GetMapping("/find/{ID}")
+    public PersonAdminDTO findById(@PathVariable(value = "ID") Long id) {
 
-    @PostMapping()
-    public PersonAdminDTO createPerson(@RequestBody PersonAdminDTO item) {
-        return service.create(item);
+        PersonEntity orElse = personsRepository.findAll(id).orElse();
+        assert orElse != null;
+        return personMapper.entityToDto(orElse);
+
     }
+    @PostMapping("/save")
+    public PersonAdminDTO save(@RequestBody PersonAdminDTO personAdminDTO) {
 
-    @PutMapping("/{id}")
-    public PersonAdminDTO updatePerson(@PathVariable Long id, @RequestBody PersonAdminDTO item) {
-        return service.update(id, item);
-    }
+        PersonEntity personEntity = personMapper.dtoToEntity(personAdminDTO);
+        personEntity = personsRepository.save(personEntity);
+        return personMapper.entityToDto(personEntity);
 
-    @DeleteMapping("/{id}")
-    public Boolean deletePerson(@PathVariable Long id) {
-        return service.delete(id);
     }
 
 }
