ddf5ac0669f0c438942960b8ef715f24e10bbdf8
s6raphin <s6raphin@laposte.net>
2021-03-30 14:05:20 +0200

    #20 Création repository "Persons"
    Création DtoToPersonMapper

--- src/main/java/com/safetynet/safetynetalerts/mapper/PersonInfoDtoToPersonMapper.java
+++ src/main/java/com/safetynet/safetynetalerts/mapper/PersonInfoDtoToPersonMapper.java
@@ -2,7 +2,9 @@ package com.safetynet.safetynetalerts.mapper;
 
 import com.safetynet.safetynetalerts.dto.PersonInfoDTO;
 import com.safetynet.safetynetalerts.entity.PersonEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface PersonInfoDtoToPersonMapper {
 
     Iterable<PersonInfoDTO> entityToDto(Iterable<PersonEntity> personList);
