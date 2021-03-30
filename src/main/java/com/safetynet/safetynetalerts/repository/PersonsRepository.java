14bb561ee561335b9477047219f09222a41eb235
s6raphin <s6raphin@laposte.net>
2021-03-30 14:05:42 +0200

    #20 Création repository "Persons"
    Création repository

--- src/main/java/com/safetynet/safetynetalerts/repository/PersonsRepository.java
+++ src/main/java/com/safetynet/safetynetalerts/repository/PersonsRepository.java
@@ -1,15 +1,17 @@
 package com.safetynet.safetynetalerts.repository;
 
 import com.safetynet.safetynetalerts.entity.PersonEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Id;
+import java.util.Optional;
+import java.util.OptionalLong;
 
 @Repository
-public interface PersonsRepository extends CrudRepository<PersonEntity, Long> {
+public interface PersonsRepository extends JpaRepository<PersonEntity, Long> {
 
-
-    
+    OptionalLong findAll(Long id);
 
 }
