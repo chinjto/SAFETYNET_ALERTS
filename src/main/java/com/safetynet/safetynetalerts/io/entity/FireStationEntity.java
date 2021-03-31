fed1b98d6c9c5385f99aeafa00bcf7716c91005b
chinjto <contact@cyril-defaye.fr>
2021-03-31 04:55:12 +0200

    #11 #12 #13 Endpoints POST/PUT/DELETE http://localhost:8080/firestation
    Ajout des méthodes equals et attributs permettant d'identifier une entité

--- src/main/java/com/safetynet/safetynetalerts/io/entity/FireStationEntity.java
+++ src/main/java/com/safetynet/safetynetalerts/io/entity/FireStationEntity.java
@@ -2,13 +2,16 @@ package com.safetynet.safetynetalerts.io.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class FireStationEntity {
 
+    @EqualsAndHashCode.Include
     private String address;
 
     private String station;
--- src/main/java/com/safetynet/safetynetalerts/io/entity/PersonEntity.java
+++ src/main/java/com/safetynet/safetynetalerts/io/entity/PersonEntity.java
@@ -2,6 +2,7 @@ package com.safetynet.safetynetalerts.io.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
@@ -9,10 +10,13 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class PersonEntity implements Serializable {
 
+    @EqualsAndHashCode.Include
     private String firstName;
 
+    @EqualsAndHashCode.Include
     private String lastName;
 
     private String address;
